f9fb8446a5eb79d169e23c03a266e51e0b85ce67
Ted Neff <tedneffv@gmail.com>
2022-12-11 14:33:03 -0700

    part 2 day 4 complete

--- adventOfCodeDay4/adventOfCodeDay4.js
+++ adventOfCodeDay4/adventOfCodeDay4.js
@@ -3,8 +3,29 @@ const { readFile } = require("fs");
 readFile('data.txt', (err, data) => {
     if (err) throw err;
 
-    // this is at test
-    const dataArray = data.toString().split('\n')
-    console.log({ dataArray: dataArray[1] });
+    const elfGroupArray = data.toString().split('\n')
+    let completeOverlapCount = 0;
+    let completeOverlapCount2 = 0;
+    elfGroupArray.forEach((group) => {
+        const elfGroup = group.split(',');
+        const firstGroup = elfGroup[0].split('-').map((el) => +el);
+        const secondGroup = elfGroup[1].split('-').map((el) => +el);
+        /* Part 1 */
+        if ((firstGroup[0] <= secondGroup[0] && firstGroup[1] >= secondGroup[1]) || 
+        (+secondGroup[0] <= firstGroup[0] && secondGroup[1] >= firstGroup[1])) {
+            completeOverlapCount++
+        }
+        /* Part 2 */
+        const maxIndex = Math.max(firstGroup[0], firstGroup[1], secondGroup[0], secondGroup[1]);
+        for (let i = 0; i <= maxIndex; i++) {
+            if (i >= firstGroup[0] && i <= firstGroup[1] && i >= secondGroup[0] && i <= secondGroup[1]) {
+                completeOverlapCount2++
+                break
+            }
+        }
+    })
+    
+    console.log({ completeOverlapCount });
+    console.log({ completeOverlapCount2 });
 });
 
